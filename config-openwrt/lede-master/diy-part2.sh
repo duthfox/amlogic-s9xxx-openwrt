a8da9b896ffc8a1591bcd5ac8679c90859de87ce
duthfox <g1226cl@gmail.com>
2023-04-27 18:22:55 +0800

    Update diy-part2.sh

--- config-openwrt/lede-master/diy-part2.sh
+++ config-openwrt/lede-master/diy-part2.sh
@@ -7,6 +7,7 @@
 #========================================================================================================================
 
 # ------------------------------- Main source started -------------------------------
+
 #
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
@@ -20,6 +21,7 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.150/g' package/base-files/files/bin/config_generate
 
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
